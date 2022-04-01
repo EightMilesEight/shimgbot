e6eb411d87696ef6ec4676e0ef413715fa10b3b1
Uchiha132 <102783221+Uchiha132@users.noreply.github.com>
2022-03-31 23:47:03 -0700

    Add more code

--- bot.sh
+++ bot.sh
@@ -6,7 +6,12 @@ randNextLoop=1
 #Skip waiting altogether and run as fast as possible.
 noWait=false
 #Size of captured image from screen.
-pos=400x200+0+0
+width=10
+height=10
+pos=${width}x${height}+0+0
+
+#Copy over pixel data from picture to be created.
+tux=$(convert tux.png sparse-color: | tr ' ' '\n')
 
 while true; do
     timer=$(((RANDOM % $randNextLoop) + $nextLoop))
@@ -14,5 +19,28 @@ while true; do
     test $noWait = true || sleep $timer
     echo 'do some stuff with xdotool and imagemagick'
     data="$(import -window root -crop "$pos" png:- | convert - sparse-color: | tr ' ' '\n')"
-    echo "$data"
+
+    i=0
+    x=0
+    y=0
+        echo "$data"
+    echo "$data" | while read -r line; do 
+        i=$((i + 1))
+
+        x=$(echo "$line" | cut -d',' -f1)
+        y=$(echo "$line" | cut -d',' -f2)
+        color=$(echo "$line" | cut -d',' -f3-)
+       
+        tuxLine=$(echo "$tuxLine" | sed -n ${i}p)
+        tuxX=$(echo "$tuxLine" | cut -d',' -f1)
+        tuxY=$(echo "$tuxLine" | cut -d',' -f2)
+        tuxColor=$(echo "$tuxLine" | cut -d',' -f3-)
+
+        echo $x $y $color
+        echo $tuxX $tuxY $tuxColor
+        #xdotool mousemove $x $y click 1
+    done
+    i=0
+    x=0
+    y=0
 done
